ac1460e85b99f2800d2c3f006ca67a93217eec4f
adityagoyal <goyaladi>
2020-05-13 13:41:56 +0530

    schedule file added

---
+++ schedule.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+table_name = 'active_flights'
+
+engine = create_engine("postgres://wmbcvvjiskfgau:e516ae997a2f8dfb1c29941e8fab818b76d17672865106f090328c7f5ad4d414@ec2-50-17-21-170.compute-1.amazonaws.com:5432/d3c33hv0lpcnck")
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def main():
+    
+    flights = db.execute("SELECT * FROM {}".format(table_name)).fetchall()
+    return render_template("flights.html", flights=flights)
+
+if __name__ == "__main__":
+    main()
