a9752e81356e2c986147c97ad9759cd0b778dc49
adityagoyal <goyaladi>
2020-05-13 17:50:03 +0530

    Booking feature

--- schedule.py
+++ schedule.py
@@ -8,9 +8,21 @@ app = Flask(__name__, template_folder='templates')
 table_name = 'active_flights'
 engine = create_engine("postgres://wmbcvvjiskfgau:e516ae997a2f8dfb1c29941e8fab818b76d17672865106f090328c7f5ad4d414@ec2-50-17-21-170.compute-1.amazonaws.com:5432/d3c33hv0lpcnck")
 db = scoped_session(sessionmaker(bind=engine))
+flights = db.execute("SELECT * FROM active_flights").fetchall()
 
 @app.route("/")
 def index():
 
-    flights = db.execute("SELECT * FROM active_flights").fetchall()
     return render_template("flights.html", flights=flights)
+
+@app.route("/book")
+def book():
+    return render_template("booking.html", flights=flights)
+
+@app.route("/complete_booking", methods=["POST"])
+def complete_booking():
+    origin = request.method.get("origin")
+    destination = request.method.get("destination")
+
+    print(origin, destination, "PRINTED")
+
